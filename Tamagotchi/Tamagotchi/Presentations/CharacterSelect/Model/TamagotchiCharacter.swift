5d1e044ab4c5bbcbf5bc520acc85971d9ed87976
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-02-20 21:14:51 +0900

    feat: 다마고치 모델 정의 및 mock 객체 선언

---
+++ Tamagotchi/Tamagotchi/Presentations/CharacterSelect/Model/TamagotchiCharacter.swift
@@ -0,0 +1,137 @@
+//
+//  TamagotchiCharacter.swift
+//  Tamagotchi
+//
+//  Created by 조성민 on 2/20/25.
+//
+
+struct TamagotchiCharacter {
+    let name: String
+    let imageName: String
+}
+
+struct TamagotchiCharacters {
+    // 30개
+    let list: [TamagotchiCharacter] = [
+        TamagotchiCharacter(
+            name: "따끔따끔 다마고치",
+            imageName: "1-6"
+        ),
+        TamagotchiCharacter(
+            name: "방실방실 다마고치",
+            imageName: "2-6"
+        ),
+        TamagotchiCharacter(
+            name: "반짝반짝 다마고치",
+            imageName: "3-6"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        ),
+        TamagotchiCharacter(
+            name: "준비중이에요",
+            imageName: "noImage"
+        )
+    ]
+}

86ee59fb863b5dc9c26dc9b5a1f034aba1098fdf
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-02-20 21:42:58 +0900

    feat: UserDefaultsManager, PropertyWrapped UserDefault 구현

---
+++ Tamagotchi/Tamagotchi/Utils/UserDefaultsManager.swift
@@ -0,0 +1,28 @@
+//
+//  UserDefaultsManager.swift
+//  Tamagotchi
+//
+//  Created by 조성민 on 2/20/25.
+//
+
+import Foundation
+
+final class UserDefaultsManager {
+    private enum UserDefaultsKey: String, CaseIterable {
+        case isOnboardingDone
+    }
+
+    static let shared = UserDefaultsManager()
+    
+    @UserDefault(
+        key: UserDefaultsKey.isOnboardingDone.rawValue,
+        defaultValue: false
+    )
+    var isOnboardingDone: Bool
+    
+    func resetUserData() {
+        UserDefaultsKey.allCases.forEach { key in
+            UserDefaults.standard.removeObject(forKey: key.rawValue)
+        }
+    }
+}

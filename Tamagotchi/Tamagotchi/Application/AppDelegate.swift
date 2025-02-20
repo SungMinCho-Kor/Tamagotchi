91ec8f63004a6e59df58598e7f2eab0f39f99a01
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-02-20 17:06:56 +0900

    build: 프로젝트 초기 세팅

---
+++ Tamagotchi/Tamagotchi/Application/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tamagotchi
+//
+//  Created by 조성민 on 2/20/25.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}

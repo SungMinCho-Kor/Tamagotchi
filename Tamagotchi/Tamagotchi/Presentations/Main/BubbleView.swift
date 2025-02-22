79e091e40833f246963dbf2936b862953cd15e28
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-02-22 14:08:40 +0900

    feat: BubbleView 구현

---
+++ Tamagotchi/Tamagotchi/Presentations/Main/BubbleView.swift
@@ -0,0 +1,41 @@
+//
+//  BubbleView.swift
+//  Tamagotchi
+//
+//  Created by 조성민 on 2/22/25.
+//
+
+import UIKit
+import SnapKit
+
+final class BubbleView: BaseView {
+    private let bubbleImageView = UIImageView(image: .bubble)
+    let contentLabel = UILabel()
+    
+    override func configureHierarchy() {
+        [
+            bubbleImageView,
+            contentLabel
+        ].forEach(addSubview)
+    }
+    
+    override func configureLayout() {
+        bubbleImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        contentLabel.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(60)
+        }
+    }
+    
+    override func configureViews() {
+        contentLabel.textColor = .servicePrimary
+        contentLabel.textAlignment = .center
+        contentLabel.font = .systemFont(ofSize: 14)
+        
+        bubbleImageView.contentMode = .scaleAspectFill
+    }
+}
+

4c29148ee2a4129fde3e8a9fd5022f580065f310
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-02-24 01:53:49 +0900

    fix: Level 1부터 시작으로 수정

--- Tamagotchi/Tamagotchi/Presentations/CharacterSelect/Model/CharacterInformation.swift
+++ Tamagotchi/Tamagotchi/Presentations/CharacterSelect/Model/CharacterInformation.swift
@@ -8,16 +8,19 @@
 struct CharacterInformation: Codable {
     var character: TamagotchiCharacter
     var level: Int {
-        let calculatedLevel = Int(Double(food) / 5 + (Double(water) / 2)) / 10
-        return calculatedLevel >= 10 ? 10 : calculatedLevel
+        var calculatedLevel = Int(Double(food) / 5 + (Double(water) / 2)) / 10
+        if calculatedLevel < 1 {
+            calculatedLevel = 1
+        } else if calculatedLevel > 10 {
+            calculatedLevel = 10
+        }
+        return calculatedLevel
     }
     var water: Int
     var food: Int
     var imageName: String {
         if let number = character.number {
-            if level <= 1 {
-                return "\(number)-1"
-            } else if level < 9 {
+            if level < 9 {
                 return "\(number)-\(level)"
             } else {
                 return "\(number)-9"
